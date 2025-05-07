f5615258cf5e87e2f024231e08e9e5087dee7f86
Pereirapsdf <jpg.pereir4@gmail.com>
2025-05-07 11:05:54 -0300

    d

--- UIJPG529/Form2.cs
+++ UIJPG529/Form2.cs
@@ -12,6 +12,8 @@ namespace UIJPG529
 {
     public partial class Form2 : Form
     {
+        DominioJPG529.Users User = new DominioJPG529.Users();
+        DallJPG529.Mozo529 Mozo = new DallJPG529.Mozo529();
         public Form2()
         {
             InitializeComponent();
@@ -29,5 +31,31 @@ namespace UIJPG529
             Form1 form1 = new Form1();
             form1.Show();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(textBox1.Text) || string.IsNullOrWhiteSpace(textBox2.Text) || string.IsNullOrWhiteSpace(textBox3.Text) || string.IsNullOrWhiteSpace(textBox3.Text))
+                {
+                    MessageBox.Show("Por favor, ingrese un texto.");
+                }
+                else
+                {
+                    User.NombreUsuario = textBox1.Text;
+                    User.Contraseña = textBox2.Text;
+                    User.Dni = Convert.ToInt16(textBox3.Text);
+                    Mozo.Update(User.NombreUsuario, User.Dni, User.Contraseña, textBox4.Text);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
     }
-}
+ }
+
